5a07141cee31743a6c20a9e203a223dbe6f9cebf
abdelrhman8889@gmail.com <abdelrhman8889@gmail.com>
2021-04-26 22:45:35 +0200

    done

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const shoopingCart = [];
+const products = [
+    {
+        id: "20180306",
+        productName: "samsung A70",
+        category: "phone"
+
+    },
+    {
+        id: "20180302",
+        productName: "iphone 12 pro max",
+        category: "phone"
+    },
+    {
+        id: "20180452",
+        productName: "iphone 12 ",
+        category: "phone"
+    }
+]
+
+
+app.use(bodyParser.json());
+
+// retrive all products
+app.get("/products", (req, res) => {
+    res.json(products)
+})
+// retrive product by ID 
+app.get("/products/:id", (req, res) => {
+    res.json(products.filter(el => el.id === req.params.id))
+})
+// add new product
+app.post("/products", (req, res) => {
+    products.push(res.body)
+    res.send(products)
+})
+// add product to shopping cart 
+app.post("/products", (req, res) => {
+    shoopingCart.push(res.body)
+    res.send(shoopingCart)
+})
+
+app.listen(3200, () => {
+    console.log("Server running....");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
